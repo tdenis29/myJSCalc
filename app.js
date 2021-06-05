fa15472b1743ea89fd44db85eee846740fc47f48
tdenis29 <tdenis29@outlook.com>
2021-06-05 15:57:21 -0700

    thatonlytookALLDAY

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 
 
 //Vairables
-const btns = document.getElementById("buttons-grid");
+const numBtns = document.querySelectorAll("#numBtn")
+const actionBtns = document.querySelectorAll('#actionBtn')
 const display = document.getElementById('screen');
 const expression = document.getElementById("topscreen");
-let a;
-let b;
-let operator;
+console.log(numBtns)
+console.log(actionBtns)
+
 //event listener for btns or use bubbling on parent? or nested event listener? for input?
-btns.addEventListener('click', e =>{
+numBtns.forEach(button => {
+  button.addEventListener('click', e =>{
     //make sure click is on button
     if (e.target.matches("button")){
         //set clicked button to variable
@@ -18,67 +20,48 @@ btns.addEventListener('click', e =>{
         //get key content to display on screen later
         const keyContent = key.textContent;
         //display on screen 
-        const displayedNum = display.textContent;
-        //if not action key then number and display
-       
-            //if starting number is 0 then add clicked number
- 
-
-        //if this is not an action key firtst
-        // if(!action){
-        //     console.log(key)
-        // }
-        // if (
-        //     action === 'add' ||
-        //     action === 'subtract' ||
-        //     action === 'multiply' ||
-        //     action === 'divide'
-        //   ) {
-        //     console.log('operator key!')
-        //     let a = displayedNum
-        //     let operator = e.target.keyContent
-        //     display.textContent = a + keyContent;
-        //   }
-        // if(!action){
-        //     let b = key + 
-        // }
-        //   if (action === 'decimal') {
-        //     console.log('decimal key!')
-        //     display.textContent = displayedNum + '.'
-        //   }
-        //   if (action === 'clear') {
-        //     console.log('clear key!')
-        //   }
-        //   if (action === 'equals') {
-            
-        //     console.log('equal key!')
-        //   }
-    //   if the action keys are pressed and displayed number is 0 replace 0 then concatenate chosen num to displayNum
-    //       if (!action) {
-    //           //first key pressed replace 0
-    //         if (displayedNum === '0') {
-    //         display.textContent = keyContent
-    //         //continue to ADD number to display as they are pressed. but adds clear and delete as well
-    //         } else {
-    //            display.textContent = displayedNum + keyContent 
-    //         }   
-    //     }
-    //     if(e.target === action){
-    //         let a = displayedNum;
-    //          display.textContent = displayedNum + keyContent 
-    //     }
-    // }
-
-
+        displayedNum = display.textContent;
+        if (!action) {
+          if (displayedNum === '0') {
+            display.textContent = keyContent
+          } else {
+              display.textContent = displayedNum + keyContent
+            }
+        }
+    }}); 
+  })
 
 // add event listener ? 
-
+actionBtns.forEach(button => {
+  button.addEventListener('click', e => {
+    const key = e.target;
+    const keyContent = key.textContent;
+    if(key.dataset.action == "add" ||
+      key.dataset.action == "subtract" ||
+      key.dataset.action == 'multiply' ||
+      key.dataset.action == 'divide' 
+    )
+    {
+    displayedNum = display.textContent
+     a = displayedNum 
+     expression.textContent = a + keyContent;
+     operator = keyContent;
+     display.textContent = "";
+    }
+    if(e.target.dataset.action == "equals"){
+      displayedNum = display.textContent
+      b = displayedNum
+      expression.textContent += b;
+      display.textContent = operate(a, b, operator) 
+    }
+  })
+})
 
 
 //operand function
 function operate(a, b, operator){
     if(operator === "+"){
-       return add(a,b)
+        return add(a,b)
     } else if (operator === "-"){
         return subtract(a,b)
     } else if (operator === "*"){
@@ -91,14 +74,15 @@ function operate(a, b, operator){
 // Basic Functions
 
 const add = function(a,b) {
-	return sum = a + b;
+  
+	return parseInt(a) + parseInt(b);
 };
 
 const subtract = function(a, b) {
-	return sum = a - b;
+	return  a - b;
 };
 const divide = function(a,b){
-    return sum = a / b;
+    return  a / b;
 };
 const sum = function(arr) {
 	let total = 0;
@@ -109,7 +93,7 @@ const sum = function(arr) {
 };
 
 const multiply = function(arr) {
-  return sum = arr.reduce((a, b)=>  a*b, 1)
+  return  arr.reduce((a, b)=>  a*b, 1)
 };
 
 const power = function(a, b) {
